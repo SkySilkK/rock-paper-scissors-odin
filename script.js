8f01d965cc39741c80ac331b2eca302d2289dc49
SkySilk <79651976+SkySilkK@users.noreply.github.com>
2025-03-27 08:04:19 +0000

    fix: Update choices to 'rock', 'paper', 'scissors' for case insensitivity

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let userChoice = 'r'
+let userChoice = 'rock'
 let randomizer = Math.floor(Math.random() * 3)
-let computerChoice = 'r'
+let computerChoice = 'Rock'.toLowerCase()
 let humanScore = 0;
 let computerScore = 0;
 
@@ -9,37 +9,36 @@ function getHumanChoice() {
 
 }
 
-
 function getComputerChoice(theChoice) {
     switch (theChoice) {
         case 0:
-            return 'r'
+            return 'rock'
             break;
         case 1:
-            return 'p'
+            return 'paper'
             break;
         case 2:
-            return 's'
+            return 'scissors'
     }
 }
 
 if (
-    (computerChoice === 'r' && userChoice === 's') ||
-    (computerChoice === 'p' && userChoice === 'r') ||
-    (computerChoice === 's' && userChoice === 'p')
+    (computerChoice === 'rock' && userChoice === 'scissors') ||
+    (computerChoice === 'paper' && userChoice === 'rock') ||
+    (computerChoice === 'scissors' && userChoice === 'paper')
 ) {
     console.log("You Lose")
 } else if (
-    (computerChoice === 's' && userChoice === 'r') ||
-    (computerChoice === 'r' && userChoice === 'p') ||
-    (computerChoice === 'p' && userChoice === 's')
+    (computerChoice === 'scissors' && userChoice === 'rock') ||
+    (computerChoice === 'rock' && userChoice === 'paper') ||
+    (computerChoice === 'paper' && userChoice === 'scissors')
 ) {
     console.log("You Win")
 }
 else if(
-    (computerChoice === 's' && userChoice === 's') ||
-    (computerChoice === 'r' && userChoice === 'r') ||
-    (computerChoice === 'p' && userChoice === 'p')
+    (computerChoice === 'scissors' && userChoice === 'scissors') ||
+    (computerChoice === 'rock' && userChoice === 'rock') ||
+    (computerChoice === 'paper' && userChoice === 'paper')
 ) {
     console.log("It's a tie!")
 } else {
