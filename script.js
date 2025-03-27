0428ec634f5d1c1831e58f213894b66a1188de3a
SkySilk <79651976+SkySilkK@users.noreply.github.com>
2025-03-27 07:46:46 +0000

    fix: Add tie handling to rsp
    Fixes a bug where when both userChoice and computerChoice are the same it show a tie instead of 'enter correct values'

--- script.js
+++ script.js
@@ -1,45 +1,49 @@
-// Online Javascript Editor for free
-// Write, Edit and Run your Javascript code using JS Online Compiler
-
 let userChoice = 'r'
-let randomizer = Math.floor(Math.random()*3)
-let computerChoice = getComputerChoice(randomizer)
+let randomizer = Math.floor(Math.random() * 3)
+let computerChoice = 'r'
 let humanScore = 0;
 let computerScore = 0;
 
 // Leaving getHumanChoice empty for future use
-function getHumanChoice(){
-    
+function getHumanChoice() {
+
 }
 
 
-function getComputerChoice(theChoice){
+function getComputerChoice(theChoice) {
     switch (theChoice) {
-    case 0:
-        return 'r'
-        break;
-    case 1:
-        return 'p'
-        break;
-    case 2:
-        return 's'
-   }
+        case 0:
+            return 'r'
+            break;
+        case 1:
+            return 'p'
+            break;
+        case 2:
+            return 's'
+    }
 }
 
-if ( 
-     (computerChoice==='r' && userChoice==='s') ||
-     (computerChoice==='p' && userChoice==='r') ||
-     (computerChoice==='s' && userChoice==='p') 
-     ) {
+if (
+    (computerChoice === 'r' && userChoice === 's') ||
+    (computerChoice === 'p' && userChoice === 'r') ||
+    (computerChoice === 's' && userChoice === 'p')
+) {
     console.log("You Lose")
-  } else if(
-     (computerChoice==='s' && userChoice==='r') ||
-     (computerChoice==='r' && userChoice==='p') ||
-     (computerChoice==='p' && userChoice==='s')
-    ) {
+} else if (
+    (computerChoice === 's' && userChoice === 'r') ||
+    (computerChoice === 'r' && userChoice === 'p') ||
+    (computerChoice === 'p' && userChoice === 's')
+) {
     console.log("You Win")
-    } else {
-      console.log("Enter correct values")
-  }
-  
+}
+else if(
+    (computerChoice === 's' && userChoice === 's') ||
+    (computerChoice === 'r' && userChoice === 'r') ||
+    (computerChoice === 'p' && userChoice === 'p')
+) {
+    console.log("It's a tie!")
+} else {
+    console.log("Enter correct values")
+}
+
 console.log(computerChoice)
